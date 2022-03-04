53e8c69d01aef52062218839145717c55127ab05
Mariasworld <mashalifeworld@gmail.com>
2022-03-04 17:14:16 +0300

    lesson2

---
+++ src/main/java/lesson2.java
@@ -0,0 +1,18 @@
+public class HomeWorkApp {
+    public static boolean PrintTF(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        if (sum >= 10) {
+            if (sum <= 20) {
+                System.out.println(true);
+            } else {
+                System.out.println(false);
+            }
+        } else {
+            System.out.println(false);
+        }
+    }
+}
+
+
